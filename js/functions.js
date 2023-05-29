85abf360e720162601c0787742a1f9255bd6f78a
lilileiva <liliana-carmen@hotmail.com>
2023-05-29 18:43:04 -0300

    fix functions: minor changes

--- js/functions.js
+++ js/functions.js
@@ -5,18 +5,19 @@ let operationValue = "0"
 let openParenthesis = false
 let darkMode = true
 
-const changeTheme = () => {    
-    darkMode == true ? darkMode = false : darkMode = true
+const changeTheme = () => {
     if (darkMode == true) {
         container.className = "lightMode"
         calcBody.className = "lightCalcBody"
         btnTheme.className = "lightBtnTheme"
         btnTheme.textContent = 'Dark mode'
+        darkMode = false
     } else {
         container.className = "darkMode"
         calcBody.className = "darkCalcBody"
         btnTheme.className = "darkBtnTheme"
         btnTheme.textContent = 'Light mode'
+        darkMode = true
     }
 }
 
@@ -40,16 +41,17 @@ const cleanScreen = () => {
     operationValue = "0"
     operation.textContent = operationValue
     result.textContent = ""
+    openParenthesis = false
 }
 
 let number_keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-let symbol_keys = ["+", "-", "*", "/", "(", ")", "."]
+let math_symbols = ["+", "-", "*", "/", "."]
 
 const onKeyPress = (e) => {    
-    if (e.key in number_keys) operationValue == "0" ? operationValue = e.key : operationValue += e.key
+    if (e.key in number_keys) operationValue == "0" ? operationValue = e.key : operationValue += e.key    
     if (e.key == "+" || e.key == "-" || e.key == "*" || e.key == "/" || e.key== ".") operationValue += e.key
-    if (e.key == "(" && openParenthesis == false) {
-        if (operationValue.slice(-1) != ("+" && "-" && "*" && "/" && ".")) operationValue += "*"
+    if (e.key == "(" && openParenthesis == false) {        
+        if (operationValue.slice(-1) in math_symbols) operationValue += "*"
         operationValue += e.key
         openParenthesis = true
     }
@@ -65,12 +67,12 @@ const onKeyPress = (e) => {
 const clickedButton = (e) => {
     if (e.target.textContent in number_keys) operationValue == "0" ? operationValue = e.target.textContent : operationValue += e.target.textContent    
     if (e.target.textContent == "+" || e.target.textContent == "-" || e.target.textContent == "*" || e.target.textContent == "/" || e.target.textContent== ".") operationValue += e.target.textContent
-    if (e.target.textContent == "(" && openParenthesis == false) {
-        if (operationValue.slice(-1) != ("+" && "-" && "*" && "/" && ",")) operationValue += "*"
+    if (e.target.textContent == "(" && openParenthesis == false) {        
+        if (operationValue.slice(-1) in math_symbols) operationValue += "*"
         operationValue += e.target.textContent
         openParenthesis = true
     }
-    if (e.target.textContent == ")" && (operationValue.slice(-1) != ("+" && "-" && "*" && "/" && ",")) && openParenthesis == true) {
+    if (e.target.textContent == ")" && (operationValue.slice(-1) != ("+" && "-" && "*" && "/" && ".")) && openParenthesis == true) {
         operationValue += e.target.textContent
         openParenthesis = false
     }
